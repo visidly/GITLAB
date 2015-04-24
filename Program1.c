713e4f9522d3a7d3aeb18c274fbcdebd98d1178c
ployCPE <k.phanomphorn@gmail.com>
2015-04-24 21:06:16 +0700

    UserB naja

--- Program1.c
+++ Program1.c
@@ -2,6 +2,7 @@
 
 void main()
 {
-	printf("Hello from program 1");
+	printf("Hello from program 1, userB change for no.18 na");
 	printf("User A change this eiei");
+	printf("User B change this later na eiei");
 }
