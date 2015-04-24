ae0f0a1823142eb19b40859196c8c67d509804ad
visidly <visidly@hotmail.com>
2015-04-24 21:04:15 +0700

    change by user A

--- Program1.c
+++ Program1.c
@@ -2,6 +2,8 @@
 
 void main()
 {
-	printf("Hello from program 1");
+	printf("Hello from program 1, user A change this na");
 	printf("User A change this eiei");
+
+	printf("User A want to print this");
 }
